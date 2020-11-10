c7d422e3de23f74c270a91c139b2d3034325875a
MezianeKhalil <khalilmez2000@gmail.com>
2020-11-10 13:26:48 +0100

    firts commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  future: {
+    // removeDeprecatedGapUtilities: true,
+    // purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {},
+    fontFamily:{
+      'Poppins':["Poppins","sans-serif"]
+    }
+  },
+  variants: {},
+  plugins: [],
+}
