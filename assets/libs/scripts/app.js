701b9d2e7d54f346311b6df0572e68ba15f272cd
MezianeKhalil <khalilmez2000@gmail.com>
2020-11-11 13:56:27 +0100

    new changes

--- assets/libs/scripts/app.js
+++ assets/libs/scripts/app.js
@@ -1,10 +1,21 @@
 let images = document.querySelectorAll('img')
+let options = {
+    root: null ,
+    rootMargin: '250px',
+    threshold: 0 ,
+}
 let observer = new IntersectionObserver(
-    (entries)=>{
-        if(entries[0].isIntersecting){
+    (entries,observer)=>{
+        entries.forEach(entre=>{
+            if(!entre.isIntersecting){
+                return;
+            }
+            console.log(entries[0].target)
             entries[0].target.src = entries[0].target.dataset.src
-        }
+            observer.unobserve(entre.target)
+        })
     }
+    ,options
 )
 images.forEach(img=>{
     observer.observe(img)
